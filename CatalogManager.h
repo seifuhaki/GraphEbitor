4448353efa4319cfa89ddfd929ec6faac683cbc2
SuXY <44319559+SuXY98@users.noreply.github.com>
2019-06-12 14:14:16 +0800

    Update CatalogManager.h

--- CatalogManager.h
+++ CatalogManager.h
@@ -18,14 +18,13 @@
 #define TableInfoPath "CatalogInfo/TableInfo.txt"
 #define IndexInfoPath "CatalogInfo/IndexInfo.txt"
 
-// 在API中使用时只需要以下函数
-//	createTable, dropTable, createIndex, dropIndex
-//	作用同函数名，判定逻辑基本已在模块中实现，发生错误
-//	时会丢出相应的错误类型（详见更新后的exception.h）
-//	需要在API中实现的判定只有一点，就是primaryKey是否在attributeNames中
-//	因为这一项不需要直接与数据文件交互，就不放在CatalogManager里面了 
-//	所有函数都已经过测试，大体上应该没有问题，但也不排除有我没有考虑到的特殊情况
-//	如果在设计API进行测试的时候遇到了属于CatalogManager里面的错误，和我吱一声，我再debug一下 
+class IndexInfo {
+	public:
+		std::string indexName;
+		std::string tableName;
+		std::string attributeName;
+};
+
 class CatalogManager {
 	public:
 		CatalogManager();
@@ -41,6 +40,9 @@ class CatalogManager {
 		std::string num2str(int num, std::size_t length);
 		void removeChara(std::string &str, char c);
 		bool hasIndex(const std::string indexName);
+		bool isUnique(const std::string tableName, const std::string attributeName);
+		IndexInfo getIndexInfo(const std::string indexName);
+
 	private:
 		BufferManager bm;
 
