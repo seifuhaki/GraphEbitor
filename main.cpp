776b47b8f86528d4adc376271d45971f1395bfd2
SuXY <44319559+SuXY98@users.noreply.github.com>
2019-06-14 23:53:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
 #include<iostream>
 #include <string>
-#include "api.h"
+#include "Interpreter.h"
 
-int main() {
-	API api;
-	std::string tableName = "my";
-	std::string primary = "primary";
-	std::string str = "ef";
-	TableInfo tinfo;
-	tinfo.tableName = tableName;
-	tinfo.attributeNames.push_back(primary);
-	tinfo.types.push_back("string");
-	tinfo.unique.push_back(true);
-	tinfo.attributeNames.push_back(str);
-	tinfo.types.push_back("string");
-	tinfo.unique.push_back(false);
-	Table table = Table(tableName, tinfo);
-	Tuple tuple =Tuple();
-	data data_in;
-	data_in.type = "string";
-	data_in.datas = "mysql";
-	tuple.addData(data_in);
-	api.createTable(table.getTitle(), table.attr_, primary);
-	//api.insertRecord(tableName, tuple);
-	int a;
-	std::cin >> a;
-	return 0;
+int main()
+{
+	Interpreter a;
+	while (true) {
+		a.getInstruction();
+		a.executeInstruction();
+		if (a.quit()) {
+			break;
+		}
+	}
+	system("pause");
 }
