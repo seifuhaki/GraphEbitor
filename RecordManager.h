7e69950aacb7a385cae4effaddb0ae1a6eb6bc07
SuXY <44319559+SuXY98@users.noreply.github.com>
2019-06-18 20:18:57 +0800

    Update RecordManager.h

--- RecordManager.h
+++ RecordManager.h
@@ -35,30 +35,30 @@ public:
 	//异常：如果元组类型不匹配，抛出tuple_type_conflict异常。如果
 	//主键冲突，抛出primary_key_conflict异常。如果unique属性冲突，
 	//抛出unique_conflict异常。如果表不存在，抛出table_not_exist异常。
-	void insertRecord(std::string tableName, Tuple &tuple, IndexManager* im);
+	void insertRecord(std::string tableName, Tuple &tuple, IndexManager* im, CatalogManager& cm);
 	//输入：表名
 	//输出：int(删除的记录数)
 	//功能：删除对应表中所有记录（不删除表文件）
 	//异常：如果表不存在，抛出table_not_exist异常
-	int deleteRecord(std::string tableName, IndexManager* im);
+	int deleteRecord(std::string tableName, IndexManager* im, CatalogManager& cm);
 	//输入：表名，目标属性，一个Where类型的对象
 	//输出：int(删除的记录数)
 	//功能：删除对应表中所有目标属性值满足Where条件的记录
 	//异常：如果表不存在，抛出table_not_exist异常。如果属性不存在，抛出attribute_not_exist异常。
 	//如果Where条件中的两个数据类型不匹配，抛出data_type_conflict异常。
-	int deleteRecord(std::string tableName, std::string target_attr, Where where, IndexManager*im);
+	int deleteRecord(std::string tableName, std::string target_attr, Where where, IndexManager*im, CatalogManager& cm);
 	//输入：表名
 	//输出：Table类型对象
 	//功能：返回整张表
 	//异常：如果表不存在，抛出table_not_exist异常
-	Table selectRecord(std::string tableName, std::string resultTableName = "tmptable");
+	Table selectRecord(std::string tableName, CatalogManager& cm, std::string resultTableName = "tmptable");
 	//输入：表名，目标属性，一个Where类型的对象
 	//输出：Table类型对象
 	//功能：返回包含所有目标属性满足Where条件的记录的表
 	//异常：如果表不存在，抛出table_not_exist异常。如果属性不存在，抛出attribute_not_exist异常。
 	//如果Where条件中的两个数据类型不匹配，抛出data_type_conflict异常。
-    Table selectRecord(std::string table_name, std::string target_attr, Where where,IndexManager* im,std::string result_table_name = "tmp_table_name");
-	void createIndex(IndexManager* index_manager, std::string tableName, std::string target_attr);
+	Table selectRecord(std::string table_name, std::string target_attr, Where where, IndexManager* im, CatalogManager& cm, std::string result_table_name = "tmp_table_name");
+	void createIndex(IndexManager* index_manager, std::string tableName, std::string target_attr, CatalogManager& cm);
 private:
 	//获取文件大小
 	int getBlockNum(std::string table_name);
@@ -75,11 +75,10 @@ private:
 	//带索引查找
 	std::vector<int> searchWithIndex(std::string tableName, std::string attributeName, Where where, IndexManager* im);
 	//在块中进行条件删除
-	int conditionDeleteInBlock(std::string table_name, int block_id, TableInfo attr, int index, Where where, IndexManager*im);
+	int conditionDeleteInBlock(std::string table_name, int block_id, TableInfo attr, int index, Where where, IndexManager*im, CatalogManager& cm);
 	//在块中进行条件查询
 	void conditionSelectInBlock(std::string table_name, int block_id, TableInfo attr, int index, Where where, std::vector<Tuple>& v);
 
 	BufferManager bm;
-	CatalogManager cm;
 };
 #endif
